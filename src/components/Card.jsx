2bb41f00d1fa994b891a1cf7ce448c8968fae749
sarthak766 <98082282+sarthak766@users.noreply.github.com>
2024-03-02 18:58:42 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import { BsFillBagFill } from "react-icons/bs";
 import { addToCart } from '../redux/cartSlice';
 import { useDispatch } from 'react-redux';
-
 const Card = ({ id,img, title, star, reviews, prevPrice, newPrice }) => {
-  const dispatch = useDispatch();
-
+  const dispatch = useDispatch()
   const handleAddToCart = () => {
-    dispatch(addToCart({ id, img, title, newPrice, prevPrice }));
+    dispatch(addToCart({ id,img, title, newPrice,prevPrice }));
   };
-
   return (
-    <div className="max-w-md bg-white shadow-lg rounded-lg overflow-hidden mx-auto my-4 md:w-[30vw] md:h-[40vh] w-[80%] hover:scale-[1.2] transition duration-300 ease-in-out">
-      <div className='md:h-[12.5rem] md:w-[28.125rem] overflow-hidden'>
-        <img src={img} alt={title} className="w-full h-full p-4" />
-      </div>
-      <div className="p-4 md:h-[0.125rem]">
+    <div className="max-w-md bg-white shadow-lg rounded-lg overflow-hidden mx-auto my-4 md:w-[30vw] md:h-[50vh] w-[80%] hover:scale-[1.2] transition duration-300 ease-in-out">
+       <div className='md:h-[200px] md:w-[450px] overflow-hidden'>
+       <img src={img} alt={title} className="w-full h-full p-4" />
+       </div>
+      <div className="p-4 md:h-2 ">
         <h3 className="font-bold text-xl mb-2">{title}</h3>
         <div className="flex items-center mb-2">
           {/* Render star rating */}
           <section className=" flex flex-row">
-            {star} {star} {star} {star}
+            {star} {star} {star} {star} 
           </section>
           <span className="total-reviews">{reviews}</span>
         </div>
@@ -29,7 +26,7 @@ const Card = ({ id,img, title, star, reviews, prevPrice, newPrice }) => {
             <del>{prevPrice}</del> {newPrice}
           </div>
           <div>
-            <button onClick={handleAddToCart}><BsFillBagFill className="text-gray-600 h-[0.375rem] w-[0.375rem]" /></button>
+            <button onClick={handleAddToCart}><BsFillBagFill className="text-gray-600 h-6 w-6" /></button>
           </div>
         </div>
       </div>
